e65f65aca2c8ea4de2df98f04fe38e33502f2801
Igor Kandaurov <igorkandaurov@gmail.com>
2021-04-26 13:41:00 +0300

    newbranch

--- _setuser.lua
+++ _setuser.lua
@@ -1,6 +1,8 @@
 --- mqtt ---
-dat.clnt="waterboss"
-dat.brk = '192.168.1.125'
+--dat.clnt="waterboss"
+dat.clnt="test"
+--dat.brk = '192.168.1.125'
+dat.brk = '192.168.27.31'
 dat.port = 1883
 ------------
 
