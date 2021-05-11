a480dc0585b78de343a3146ff7f1884dddc96805
Igor Kandaurov <igorkandaurov@gmail.com>
2021-05-11 12:09:17 +0300

    Туча

--- timerelay.lua
+++ timerelay.lua
@@ -1,9 +1,9 @@
 local tm = rtctime.epoch2cal(rtctime.get()+3*60*60)
 local timetb = {}
-timetb[1] = {11, 18}
-timetb[2] = {11, 20}
+timetb[1] = {23, 30}
+timetb[2] = {8, 30}
 local mm = (tm.min > 9) and tm.min or ('0'..tm.min)
-print('\n\nNow '.. tm.hour..':'..mm)
+prt('\n\nNow '.. tm.hour..':'..mm)
 if wth.auto == 'On' and wth.secure == 'Off' then
     if tm.hour == timetb[1][1] and tm.min >= timetb[1][2] and wth.relay == 'On' then
         switch(0)
